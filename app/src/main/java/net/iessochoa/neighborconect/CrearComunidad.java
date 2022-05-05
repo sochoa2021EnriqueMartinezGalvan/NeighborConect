9ef89bbe274f34b31ecb9f4ef40634b56bbb013b
Enrique <equique.200@hotmail.es>
2022-05-05 12:18:03 +0200

    Se ha arreglado lo de firebase database

--- app/src/main/java/net/iessochoa/neighborconect/CrearComunidad.java
+++ app/src/main/java/net/iessochoa/neighborconect/CrearComunidad.java
@@ -16,7 +16,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 
@@ -58,17 +61,29 @@ public class CrearComunidad extends AppCompatActivity {
         String nombre = etName.getText().toString();
         String code = etCode.getText().toString();
 
-
-
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        Comunidades comunidad= new Comunidades(nombre,code);
 
-       db.collection("usr").add(user);
+        // Create a new user with a first and last name
+        Map<String, Object> comunidad = new HashMap<>();
+        comunidad.put("codigo", code);
+        comunidad.put("nombre", nombre);
+
+
+// Add a new document with a generated ID
+        db.collection("Comunidades")
+                .add(comunidad)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
 
 
         finish();
